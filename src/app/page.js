3885cd390f6e58abfabfb003a060e3aaa6b3a72f
mumbaiops45 <mumbaiops45@gmail.com>
2026-04-07 17:13:58 +0530

    Some bugs fixed

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,8 @@ export default function Home() {
   return (
 
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <CreateVendor />
+      {/* <CreateVendor /> */}
+      <p>Welcome to dashboard</p>
     </div>
   );
 }
